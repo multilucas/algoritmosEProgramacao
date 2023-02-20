7094446e198f74d5d3d03a85976c5632a8fe6065
multilucas <lucaslopes198@gmail.com>
2023-02-19 23:06:43 -0300

    init project

---
+++ PHP/sameFunctionsArray.php
@@ -0,0 +1,14 @@
+<?php
+    $cars = array("corsa","monza","corolla","tucson","mazda","fusca","uno","golf");
+    $colors = array("white","black","blue","pink","brown");
+    $heroes = array("batman","Robin","Joker",);
+    $foods = array("Fruit" => "Banana",
+        "Meat" => "Entrecote",
+        "Fast-food" => "Big-mac",
+        "Vegetable" => "Potatoes",
+        "Flowers" => "Chamomile");
+
+    print_r($foods);
+    var_dump($cars);
+
+?>
