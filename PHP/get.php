69aa167318c9225bfdb5afd5785cd5d3673f4245
multilucas <98279525+multilucas@users.noreply.github.com>
2023-02-24 00:04:31 -0300

    update exercise

--- PHP/get.php
+++ PHP/get.php
@@ -1,5 +1,12 @@
 <?php
-    echo "seu nome é " . $_GET["nome"];
+    //echo "seu nome é " . $_GET["nome"];
+
 // http://localhost/project/algoritmos/PHP/get.php?nome=Lucas
 // Esse link envia pela query string o valor do GET, no caso "Lucas"
+?>
+<?php
+    echo "Seu nome é: " . $_GET["nome"]."<br>";
+    echo "Seu telefone é: " . $_GET["tel"]."<br>";
+    echo "Seu email é: " . $_GET["email"]."<br>";
+    echo "Seu endereço é: ". $_GET["end"]."<br>";
 ?>
