c03d498716b4072fdde12cd8510e4ff1392a619e
multilucas <98279525+multilucas@users.noreply.github.com>
2023-02-23 23:55:50 -0300

    primeiro exercicio de GET

---
+++ PHP/get.php
@@ -0,0 +1,5 @@
+<?php
+    echo "seu nome é " . $_GET["nome"];
+// http://localhost/project/algoritmos/PHP/get.php?nome=Lucas
+// Esse link envia pela query string o valor do GET, no caso "Lucas"
+?>
